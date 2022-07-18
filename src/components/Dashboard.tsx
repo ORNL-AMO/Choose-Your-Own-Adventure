3da6cd64df3f2715c9db3e3060412a1d1fc175a9
JL102 <jordanlees@mailbox.org>
2022-07-18 17:10:41 -0400

    Finally got an animated gauge chart to work aaaagh

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,36 +2,56 @@ import { Box, Grid, Typography } from "@mui/material";
 import { PureComponentIgnoreFuncs } from "../functions-and-types";
 import { PaperGridItem } from "./theme";
 import React from 'react';
-import Example from "./Example";
+import GaugeChart from "./GaugeChart";
+import { theme } from "./theme";
 
 export class Dashboard extends PureComponentIgnoreFuncs <DashboardProps> {
 	render() {
+		const MAX_FINANCES = 1_500_000;
+		const MAX_CARBON_EMISSIONS = 75_000;
+		const CHART_SIZE = 250;
+		const MAX_REBATES = 500_000;
+		
 		return (
 			<>
 				<Box m={2}>
-					<Example width={500} height={500} />
-					{/* <Grid container spacing={2}>
+					<Grid container spacing={2}>
 						<Grid item xs={12}>
 							<Typography variant='h3'>Dashboard</Typography>
+							<GaugeChart 
+								width={CHART_SIZE} 
+								value={clampRatio(this.props.carbonReduced, 100)} 
+								text={`${this.props.carbonReduced.toLocaleString('en-US')}%`} 
+								label='Carbon reduction'
+								color={theme.palette.primary.dark}
+							/>
+							<GaugeChart 
+								width={CHART_SIZE} 
+								value={clampRatio(this.props.carbonEmissions, MAX_CARBON_EMISSIONS)} 
+								text={`${(this.props.carbonEmissions / 1000).toLocaleString('en-US')} kT`} 
+								label='Carbon emissions'
+								color={theme.palette.primary.dark}
+							/>
+							<GaugeChart 
+								width={CHART_SIZE} 
+								value={clampRatio(this.props.financesAvailable, MAX_FINANCES)} 
+								text={`$${this.props.financesAvailable.toLocaleString('en-US')}`} 
+								label='Finances available'
+								textFontSize={0.85}
+								color={theme.palette.secondary.dark}
+							/>
+							<GaugeChart
+								width={CHART_SIZE}
+								value={clampRatio(this.props.moneySpent, MAX_FINANCES)}
+								text={`$${this.props.financesAvailable.toLocaleString('en-US')}`}
+								label='Money spent'
+								textFontSize={0.85}
+								color={theme.palette.secondary.dark}
+							/>
 						</Grid>
 						<Grid item xs={12} sm={6}>
 							<PaperGridItem>
-								<Typography>Finances available: ${this.props.financesAvailable.toLocaleString('en-US')} / ${this.props.totalBudget.toLocaleString('en-US')}</Typography>
-							</PaperGridItem>
-						</Grid>
-						<Grid item xs={12} sm={6}>
-							<PaperGridItem>
-								<Typography>Money spent: ${this.props.moneySpent.toLocaleString('en-US')}</Typography>
-							</PaperGridItem>
-						</Grid>
-						<Grid item xs={12} sm={6}>
-							<PaperGridItem>
-								<Typography>Carbon reduction: {this.props.carbonReduced.toLocaleString('en-US')}%</Typography>
-							</PaperGridItem>
-						</Grid>
-						<Grid item xs={12} sm={6}>
-							<PaperGridItem>
-								<Typography>Carbon emissions: {this.props.carbonEmissions.toLocaleString('en-US')} metric tons</Typography>
+								<Typography>Finances available: ${this.props.financesAvailable.toLocaleString('en-US')} / ${this.props.totalBudget.toLocaleString('en-US')} budget</Typography>
 							</PaperGridItem>
 						</Grid>
 						<Grid item xs={12} sm={6}>
@@ -39,13 +59,17 @@ export class Dashboard extends PureComponentIgnoreFuncs <DashboardProps> {
 								<Typography>Rebates: ${this.props.totalRebates.toLocaleString('en-US')}</Typography>
 							</PaperGridItem>
 						</Grid>
-					</Grid> */}
+					</Grid>
 				</Box>
 				{/* todo something prettier */}
 				<hr/> 
 			</>
 		);
 	}
+}
+
+function clampRatio(value: number, max: number) {
+	return Math.min(value / max, 1);
 }
 
 export interface DashboardProps {
---
+++ src/components/GaugeChart.tsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+import { Group } from "@visx/group";
+import { Text } from '@visx/text';
+import { Arc } from "@visx/shape";
+import { animated, useSpring, to as interpolate, useTransition, config, } from '@react-spring/web';
+import { ArcProps } from "@visx/shape/lib/shapes/Arc";
+
+const defaultMargin = { top: 20, right: 20, bottom: 20, left: 20 };
+
+export type GaugeProps = {
+	/**
+	 * Width of the gauge chart. Height is automatically calculated.
+	 */
+	width: number;
+	/**
+	 * Value between 0 and 1. Not normalized.
+	 */
+	value: number;
+	text: string;
+	/**
+	 * Label at the bottom.
+	 */
+	label?: string;
+	color?: string;
+	/**
+	 * Font size of the main text. If number, it'll be a multiplier of the default size. If string, it'll be passed straight through.
+	 */
+	textFontSize?: string | number;
+	margin?: typeof defaultMargin;
+	animate?: boolean;
+};
+
+// TODO: fancy smooth animations/transitions
+
+export default function GaugeChart({
+	width,
+	margin = defaultMargin,
+	value,
+	text,
+	textFontSize,
+	label,
+	color,
+}: GaugeProps) {
+	if (width < 10) return null;
+
+	const height = width / 2 + margin.bottom;
+	const innerWidth = width - margin.left - margin.right;
+	const innerHeight = height - margin.top - margin.bottom;
+	const radius = innerWidth / 2;
+	const centerY = innerHeight;
+	const centerX = innerWidth / 2;
+	const donutThickness = radius * 0.25;
+
+	// Handle the different types of font size provided (undefined or string or number)
+	const defaultTextFontSize = Math.log(radius / 5) / Math.log(1.5) * 4.5;
+	if (typeof textFontSize === 'number') textFontSize *= defaultTextFontSize;
+	else if (!textFontSize) textFontSize = defaultTextFontSize;
+
+	return (
+		<svg width={width} height={height}>
+			<rect
+				rx={14}
+				width={width}
+				height={height}
+				fill="url('#visx-pie-gradient')"
+			/>
+			<Group top={centerY + margin.top} left={centerX + margin.left}>
+				{/* Base */}
+				<Arc
+					startAngle={-Math.PI / 2}
+					endAngle={Math.PI / 2}
+					innerRadius={radius - donutThickness}
+					outerRadius={radius}
+					fill='#ffffff'
+				/>
+				{/* Filled-in value */}
+				<AnimatedArc
+					startAngle={-Math.PI / 2}
+					endAngle={(datum) => (datum * Math.PI) - Math.PI / 2}
+					innerRadius={radius - donutThickness}
+					outerRadius={radius}
+					fill={color || 'blue'}
+					data={value}
+				/>
+					{/* {(path) => {
+						console.log(path);
+						return (
+							<animated.path
+								d={interpolate(.5, (angle) => 
+									path.path(angle)
+								)}
+								// d={path.path(0.5) || 'foo'}
+							/>
+						);
+					}} */}
+				{/* </AnimatedArc> */}
+				{/* Big text */}
+				<Text
+					verticalAnchor="end"
+					textAnchor="middle"
+					fontSize={textFontSize}
+					y={-4}
+				>
+					{text}
+				</Text>
+				{/* Label */}
+				<Text
+					verticalAnchor="start"
+					textAnchor="middle"
+					y={8}
+					fontSize='0.9rem'
+					fontWeight={200}
+					className='i'
+				>
+					{label}
+				</Text>
+			</Group>
+		</svg>
+	);
+}
+
+type AnimatedArcProps = ArcProps<number> & {
+	startAngle: any,
+	endAngle: any,
+	innerRadius: any,
+	fill: string,
+	data: number;
+}
+
+function AnimatedArc({startAngle, endAngle, innerRadius, outerRadius, fill, data}: AnimatedArcProps) {
+	
+	const [value] = useState(data);
+	
+	const transitions = useTransition(value, {
+		initial: {val: 0},
+		from: {val: 0},
+		enter: {val: data}, // When it's first rendered, I think?
+		update: {val: data}, // When the data is updated
+		delay: 200, // Delay before animating
+	});
+	
+	return (<Arc
+		{...{startAngle, endAngle, innerRadius, outerRadius, fill, data}}
+	>
+		{(arc) => {
+			return transitions((props) => {
+				return (<animated.path
+					d={interpolate([props.val], (val) => arc.path(val))}
+					fill={fill}
+				/>);
+			});
+		}}
+	</Arc>);
+}
