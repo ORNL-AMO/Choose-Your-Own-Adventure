a695ef88259dbaf079899a0ecdd8081b759d1c61
nbintertech <nblondheim@intertech.com>
2024-02-28 14:51:53 -0600

    remove godmode with offending server env var

--- src/components/SelectGameSettings.tsx
+++ src/components/SelectGameSettings.tsx
@@ -82,7 +82,7 @@ export function SelectGameSettings(props: SelectGameSettingsProps) {
                 </DialogTitle>
 
                 <DialogContent>
-                    {process.env.NODE_ENV == 'development' && 
+                    {/* {process.env.NODE_ENV == 'development' && 
                         <Box m={2} p='16px' sx={{background: '#ff000052'}}>
                             <FormGroup>
                                 <FormControlLabel control={
@@ -112,7 +112,7 @@ export function SelectGameSettings(props: SelectGameSettingsProps) {
                                 </ListItem>
                             </List>              
                         </Box>
-                    }
+                    } */}
                     <Box m={2}>
                         <DialogContentText id='alert-dialog-slide-description' gutterBottom>
                             You have the option to play through in 1 OR 2-year intervals.
