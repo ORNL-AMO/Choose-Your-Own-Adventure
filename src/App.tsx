b2613c9a5dfe25d1cdd876cb95aeb6b0d2e9e9f3
rhernandez-intertech <rhernandez@intertech.com>
2024-03-20 12:20:41 -0500

    increased NG starting val and adjusted project %'s

--- src/App.tsx
+++ src/App.tsx
@@ -677,7 +677,7 @@ export class App extends React.PureComponent<unknown, AppState> {
 	handleGameSettingsOnProceed(userSettings: UserSettings){
 		let updatingInitialTrackedStats: TrackedStats = {...initialTrackedStats};
 		let budget = 75_000;
-		let naturalGas = 120_000;
+		let naturalGas = 150_000;
 		let hydrogen = 0;
 		let electricity = 30_000_000;
 		let totalGameYears = 10;
