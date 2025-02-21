4c56fce8636d0723eee12b7a69370d4eb7f72419
Rachel Stone <rhernandez@intertech.com>
2025-02-21 12:26:48 -0600

    fixed typo

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -18,7 +18,7 @@ export function StartPage(props: StartPageProps) {
 				CHOOSE YOUR OWN SOLUTION!
 			</Typography>
 			<Typography variant='h4' component='div' gutterBottom>
-				Can you help this facility achoeve its goals?
+				Can you help this facility achieve its goals?
 			</Typography>
 			<br/>
 			<ButtonGroup 
